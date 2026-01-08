63f134babed51fce599e616d6c6518ebc9d9062a
pranshurakhecha <137769909+pranshurakhecha@users.noreply.github.com>
2026-01-08 09:06:20 +0530

    Define basic variables and arrays: thread_sum, input_vecotr, weights_vector, mul_values & set the number of threads to 10

--- obj4-perceptron-weight/main.c
+++ obj4-perceptron-weight/main.c
@@ -2,5 +2,11 @@
 #include <omp.h>
 
 int main() {
+	int i, sum = 0;
+	int thread_sum[10];
+	int input_vector[200];
+	int weights_vector[200];
+	int mul_values[200];
+	omp_set_num_threads(10);
 	return 0;
 }
