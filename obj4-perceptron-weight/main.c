d7946b416d2dd7b0de847b6398f463bdaa55b55f
pranshurakhecha <137769909+pranshurakhecha@users.noreply.github.com>
2026-01-08 09:10:02 +0530

    Implement multiprocessing logic for weight computation with results in thread_sum array for respective thread

--- obj4-perceptron-weight/main.c
+++ obj4-perceptron-weight/main.c
@@ -1,4 +1,4 @@
-#inlcude <stdio.h>
+#include <stdio.h>
 #include <omp.h>
 
 int main() {
@@ -8,5 +8,18 @@ int main() {
 	int weights_vector[200];
 	int mul_values[200];
 	omp_set_num_threads(10);
+	
+	#pragma omp parallel
+	{
+		int id = omp_get_thread_num();
+		thread_sum[id] = 0;
+		#pragma omp for
+		for(i = 0; i < 200; i++) {
+			input_vector[i] = 2 * i; 		// first 200 even numbers
+			weights_vector[i] = 2 * i + 1; 	// first 200 odd nummbers
+			mul_values[i] = input_vector[i] * weights_vector[i];
+			thread_sum[id] = thread_sum[id] + mul_values[i];
+		}
+	}
 	return 0;
 }
