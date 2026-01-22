8db785e664fdbfb5a0c3839c7ecc208cbfd5f6b2
pranshurakhecha <137769909+pranshurakhecha@users.noreply.github.com>
2026-01-22 09:18:09 +0530

    Initialize arrays arr1 and arr2 with user input

--- obj7-matrix-multiplication/main.c
+++ obj7-matrix-multiplication/main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 #include <omp.h>
 
+#define DIMENSION 3
+
 int main() {
+	int arr1[DIMENSION][DIMENSION], arr2[DIMENSION][DIMENSION], arr3[DIMENSION][DIMENSION];
+	
+	// input array 1
+	int i, j;
+	printf("Enter array 1:\n");
+	for(i = 0; i < DIMENSION; i++) 
+	{
+		printf("Enter row %d: ", i + 1);
+		for(j = 0; j < DIMENSION; j++) 
+		{	
+			scanf("%d", &arr1[i][j]);
+		}
+	}
+	
+	// input array 2
+	printf("\nEnter array 2:\n");
+	for(i = 0; i < DIMENSION; i++) 
+	{
+		printf("Enter row %d: ", i + 1);
+		for(j = 0; j < DIMENSION; j++) {
+			scanf("%d",&arr2[i][j]);
+		}
+	}
+	
+	
 	return 0;
 }
