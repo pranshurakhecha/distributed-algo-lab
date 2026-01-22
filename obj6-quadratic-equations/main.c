f1d875863a68052277e641fbecbb699826ae7f3f
pranshurakhecha <137769909+pranshurakhecha@users.noreply.github.com>
2026-01-22 08:46:56 +0530

    Input a,b,c and compute discriminant and interpret results

--- obj6-quadratic-equations/main.c
+++ obj6-quadratic-equations/main.c
@@ -3,5 +3,24 @@
 #include <omp.h>
 
 int main() {
+	int a, b, c;
+	printf("Enter a: ");
+	scanf("%d", &a);
+	printf("Enter b: ");
+	scanf("%d", &b);
+	printf("Enter c: ");
+	scanf("%d", &c);
+	
+	omp_set_num_threads(2);
+	#pragma omp parallel sections
+	{
+		#pragma omp section
+		{
+			int discriminant = (b * b) - (4 * a * c);
+			if(discriminant > 0) printf("\nThere are two roots\n");
+			else if(discriminant < 0) printf("\nThe roots are imaginary\n");
+			else printf("\nThere is a single root\n");
+		}
+	}
 	return 0;
 }
