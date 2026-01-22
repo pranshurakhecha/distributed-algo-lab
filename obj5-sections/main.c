e0bbc9443221df50d2c9ddb7297c63ec5f7be850
pranshurakhecha <137769909+pranshurakhecha@users.noreply.github.com>
2026-01-22 08:32:46 +0530

    Define threads and corresponding number of sections

--- obj5-sections/main.c
+++ obj5-sections/main.c
@@ -2,5 +2,25 @@
 #include <omp.h>
 
 int main() {
+	omp_set_num_threads(4);
+	#pragma omp parallel sections
+	{
+		#pragma omp section
+		{
+			printf("First Section\n");
+		}
+		#pragma omp section 
+		{
+			printf("Second Section\n");
+		}
+		#pragma omp section
+		{
+			printf("Third Section\n");
+		}
+		#pragma omp section 
+		{
+			printf("Fourth Section");
+		}
+	}
 	return 0;
 }
