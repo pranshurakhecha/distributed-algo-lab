63a43859918242f33543829f83346a31e54967ec
pranshurakhecha <137769909+pranshurakhecha@users.noreply.github.com>
2026-01-22 08:59:03 +0530

    Conditional root computation as per the value of discriminant in respective section

--- obj6-quadratic-equations/main.c
+++ obj6-quadratic-equations/main.c
@@ -21,6 +21,25 @@ int main() {
 			else if(discriminant < 0) printf("\nThe roots are imaginary\n");
 			else printf("\nThere is a single root\n");
 		}
+		
+		#pragma omp section 
+		{
+			int discriminant = (b * b) - (4 * a * c);
+			if(discriminant < 0);
+			else if(discriminant == 0)
+			{
+				double root = (-b) / (2.0 * a);
+				printf("Root: %.2f", root);
+			}
+			else 
+			{
+				double root1, root2;
+				root1 = ((-b) + sqrt(discriminant)) / (2.0 * a);
+				root2 = ((-b) - sqrt(discriminant)) / (2.0 * a);
+				printf("Root1: %.2f\n", root1);
+				printf("Root2: %.2f", root2);	
+			}
+		}
 	}
 	return 0;
 }
