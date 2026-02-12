891251301871d21f24bc200f677e827ad183fbbf
pranshurakhecha <137769909+pranshurakhecha@users.noreply.github.com>
2026-02-12 08:31:31 +0530

    Initialize array, set threads, and compute max in the critical section

--- obj8-max-array/main.c
+++ obj8-max-array/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <omp.h>
+#include <limits.h>
 
 int main() {
+	int arr[] = {17, 19, 2, 5, 91, 42, 49, 18, 15, 26, 37};
+	omp_set_num_threads(3);
+	int len = sizeof(arr) / sizeof(arr[0]);
+	int max = INT_MIN;
+	int i;
+	
+	#pragma omp parallel
+	{
+		for(i = 0; i < len; i++) {
+			#pragma omp critical
+			{
+				if(arr[i] > max) max = arr[i];
+			}
+		}
+	}
+	
+	printf("Maximum Number: %d", max);
 	return 0;
 }
